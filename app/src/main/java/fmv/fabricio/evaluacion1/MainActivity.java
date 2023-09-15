0b2b15b722a0c91a0b6908f1d32fc3a262775fde
FabricioMV21 <f.monsalve7@alumnos.santotomas.cl>
2023-09-14 23:07:58 -0300

    Mejora

--- app/src/main/java/fmv/fabricio/evaluacion1/MainActivity.java
+++ app/src/main/java/fmv/fabricio/evaluacion1/MainActivity.java
@@ -3,12 +3,17 @@ package fmv.fabricio.evaluacion1;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
     //Declaracion de variables
@@ -23,32 +28,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        //Nombre de aplicacion barra principal
-        getSupportActionBar().setTitle("Cotizacion");
-
         Calcular   = findViewById(R.id.btnCalcular);
         Limpiar    = findViewById(R.id.btnLimpiar);
         CVerduras  = findViewById(R.id.edtCostoV);
         CHectareas = findViewById(R.id.edtTotalH);
 
+        //Nombre de aplicacion barra principal
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Cotizacion");
 
         Calcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Validacion de campos vacios
-                if(CVerduras.getText().toString().isEmpty() || CHectareas.getText().toString().isEmpty()){
+                // Validación de campos vacíos
+                if (CVerduras.getText().toString().isEmpty() || CHectareas.getText().toString().isEmpty()) {
                     Toast.makeText(MainActivity.this, "Ingrese Los Datos Correctamente", Toast.LENGTH_SHORT).show();
-                }else{
+                } else {
                     String verduraStr = CVerduras.getText().toString();
                     String hectareaStr = CHectareas.getText().toString();
                     int v = Integer.parseInt(verduraStr);
                     int h = Integer.parseInt(hectareaStr);
-                    Intent intent = new Intent(getApplicationContext(),ResultadoActivity.class);
-                    intent.putExtra("Resultado",VerdurasPorHectareas(v,h));
+                    int calculo = VerdurasPorHectareas(v, h);
+
+                    //Guardar el resultado en SharedPreferences
+                    SharedPreferences sharedPreferences = getSharedPreferences("Calculos", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                    //Obtener la lista actual de resultados (si existe)
+                    Set<String> resultadosSet = sharedPreferences.getStringSet("resultados", new HashSet<>());
+
+                    //Calcular el índice para el nuevo cálculo
+                    int nuevoIndice = resultadosSet.size() + 1;
+
+                    //Agregar el nuevo cálculo con un prefijo indicando el índice
+                    resultadosSet.add("Resultado " + nuevoIndice + " = " + String.valueOf(calculo));
+
+                    //Guardar la lista actualizada en SharedPreferences
+                    editor.putStringSet("resultados", resultadosSet);
+                    editor.apply();
+
+                    Intent intent = new Intent(getApplicationContext(), ResultadoActivity.class);
+                    intent.putExtra("Resultado", calculo);
                     startActivity(intent);
                 }
-
             }
         });
 
@@ -60,6 +82,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
     //Metodo que genera el calulo Matematico
     public int VerdurasPorHectareas(int verduras, int hectareas){
         int resultado = verduras * hectareas;
--- app/src/main/java/fmv/fabricio/evaluacion1/ResultadoActivity.java
+++ app/src/main/java/fmv/fabricio/evaluacion1/ResultadoActivity.java
@@ -3,34 +3,62 @@ package fmv.fabricio.evaluacion1;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class ResultadoActivity extends AppCompatActivity {
     //Declaracion de variables
     TextView resultado;
     Button Volver;
+    ListView historial;
+    ImageView vaciar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
 
         Toolbar toolbar = findViewById(R.id.toolbar2);
-        setSupportActionBar(toolbar);
-        //Nombre de aplicacion barra principal
-        getSupportActionBar().setTitle("Resultados");
-
         resultado = findViewById(R.id.txtResultado);
         Volver = findViewById(R.id.btnVolver);
+        historial = findViewById(R.id.ListViewR);
+        vaciar = findViewById(R.id.ImageVaciar);
+
+        //Nombre de aplicacion barra principal
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Cotizacion");
 
         //Recibir Un Campo Numerico
         int calculo = getIntent().getIntExtra("Resultado", 0);
         resultado.setText("Total: "+ String.valueOf(calculo));
 
+        //Configuracion Del ListView para historial de calculos
+        //Obtener calculo
+        SharedPreferences sharedPreferences = getSharedPreferences("Calculos", Context.MODE_PRIVATE);
+
+        //Obtener la lista de resultados desde SharedPreferences
+        Set<String> resultadosSet = sharedPreferences.getStringSet("resultados", new HashSet<>());
+        List<String> resultadosList = new ArrayList<>(resultadosSet);
+
+        //Configurar el adaptador para el ListView
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, resultadosList);
+        historial.setAdapter(adapter);
+
+        adapter.notifyDataSetChanged();
+
+        //Configuracion Botones CLcik
         //Volver a la pantalla de inicio
         Volver.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,5 +68,26 @@ public class ResultadoActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        vaciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Limpiar el ListView
+                ArrayAdapter<String> adapter = (ArrayAdapter<String>) historial.getAdapter();
+                adapter.clear();
+                adapter.notifyDataSetChanged();
+
+                //Limpiar SharedPreferences
+                SharedPreferences sharedPreferences = getSharedPreferences("Calculos", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.clear();
+                editor.apply();
+            }
+        });
+
+
+
+
+
     }
 }
